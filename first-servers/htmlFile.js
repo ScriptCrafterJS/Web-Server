f4732b01322a92b6df79539ce7d5d4195f7a3da6
Motasem Ali <motasemali0000@gmail.com>
2024-06-16 01:08:47 +0300

    feat: include html file once
    server at startup will load the html file in a variable
    after that the server wont load the html file again for more requests

--- first-servers/htmlFile.js
+++ first-servers/htmlFile.js
@@ -1,3 +1,4 @@
+//* now we will improve the server to load the html page once the server start and save its content
 const http = require("http");
 //since we will read the html file using fs we need to load the fs module
 const fs = require("fs").promises;
@@ -5,24 +6,28 @@ const fs = require("fs").promises;
 const host = "localhost";
 const port = 8000;
 
+//store the content of the file in a variable
+let indexFile;
+
 // we will change the implementation of the requestListener function
 const requestListener = function (request, response) {
-  //the __direname will get the full path of the current directory
-  fs.readFile(__dirname + "/index.html")
-    .then((data) => {
-      response.setHeader("Content-Type", "text/html");
-      response.writeHead(200);
-      response.end(data);
-    })
-    .catch((error) => {
-      //internal error encountered and return the error message to the user
-      response.writeHead(500);
-      response.end(error.message);
-      return;
-    });
+  response.setHeader("Content-Type", "text/html");
+  response.writeHead(200);
+  response.end(indexFile);
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-  console.log(`server is running at http://${host}:${port}`);
-});
+//we shifted the work of reading the files content for the first time at the startup of the server so after that the server could handle the requests without reading the html file again
+
+//the __direname will get the full path of the current directory
+fs.readFile(__dirname + "/index.html")
+  .then((data) => {
+    indexFile = data;
+    server.listen(port, host, () => {
+      console.log(`server is running at http://${host}:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Could not read index.html file: ${error}`);
+    process.exit(1);
+  });
